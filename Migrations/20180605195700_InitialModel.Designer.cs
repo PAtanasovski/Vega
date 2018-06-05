d464e8e1da748dec629d0223eed846a1f7f16d38
Pane Atanasovski <atanasovski.pane@gmail.com>
2018-06-05 22:17:38 +0200

    Seed the database.

--- Migrations/20180605195700_InitialModel.Designer.cs
+++ Migrations/20180605195700_InitialModel.Designer.cs
@@ -8,56 +8,56 @@ using Vega.Persistence;
 
 namespace Vega.Migrations
 {
-    [DbContext(typeof(VegaDbContext))]
-    [Migration("20180605195700_InitialModel")]
-    partial class InitialModel
-    {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
-        {
+   [DbContext(typeof(VegaDbContext))]
+   [Migration("20180605195700_InitialModel")]
+   partial class InitialModel
+   {
+      protected override void BuildTargetModel(ModelBuilder modelBuilder)
+      {
 #pragma warning disable 612, 618
-            modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.0-rtm-30799")
-                .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+         modelBuilder
+             .HasAnnotation("ProductVersion", "2.1.0-rtm-30799")
+             .HasAnnotation("Relational:MaxIdentifierLength", 128)
+             .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-            modelBuilder.Entity("Vega.Models.Make", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+         modelBuilder.Entity("Vega.Models.Make", b =>
+             {
+                b.Property<int>("Id")
+                       .ValueGeneratedOnAdd()
+                       .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<string>("Name");
+                b.Property<string>("Name");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.ToTable("Makes");
-                });
+                b.ToTable("Makes");
+             });
 
-            modelBuilder.Entity("Vega.Models.Model", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+         modelBuilder.Entity("Vega.Models.Model", b =>
+             {
+                b.Property<int>("Id")
+                       .ValueGeneratedOnAdd()
+                       .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<int>("MakeId");
+                b.Property<int>("MakeId");
 
-                    b.Property<string>("Name");
+                b.Property<string>("Name");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.HasIndex("MakeId");
+                b.HasIndex("MakeId");
 
-                    b.ToTable("Model");
-                });
+                b.ToTable("Model");
+             });
 
-            modelBuilder.Entity("Vega.Models.Model", b =>
-                {
-                    b.HasOne("Vega.Models.Make", "Make")
-                        .WithMany("Models")
-                        .HasForeignKey("MakeId")
-                        .OnDelete(DeleteBehavior.Cascade);
-                });
+         modelBuilder.Entity("Vega.Models.Model", b =>
+             {
+                b.HasOne("Vega.Models.Make", "Make")
+                       .WithMany("Models")
+                       .HasForeignKey("MakeId")
+                       .OnDelete(DeleteBehavior.Cascade);
+             });
 #pragma warning restore 612, 618
-        }
-    }
+      }
+   }
 }
