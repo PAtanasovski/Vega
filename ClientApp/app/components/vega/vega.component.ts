979ec6b0f4d23ec99f00becf221611f3ceb6d632
Pane Atanasovski <atanasovski.pane@gmail.com>
2018-06-06 11:21:57 +0200

    Created new component

---
+++ ClientApp/app/components/vega/vega.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+   selector: 'vega',
+   templateUrl: './vega.component.html',
+   styleUrls: ['./vega.component.css']
+})
+export class VegaComponent implements OnInit {
+   constructor() { }
+
+   ngOnInit(): void { }
+}
