560b654336ff8fdec1f099836dc8166e1beef903
Pane Atanasovski <atanasovski.pane@gmail.com>
2018-06-06 15:49:58 +0200

    Populated dropdown lists

--- ClientApp/app/components/vega/vega.component.ts
+++ ClientApp/app/components/vega/vega.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
    selector: 'vega',
@@ -6,7 +7,41 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./vega.component.css']
 })
 export class VegaComponent implements OnInit {
-   constructor() { }
+   public makes: GetMakes[];
+   public features: GetFeatures[];
+   public models: string[];
 
-   ngOnInit(): void { }
+   constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+      http.get(baseUrl + 'api/makes')
+         .subscribe(result => {
+            this.makes = result.json() as GetMakes[];
+         }, error => console.error(error));
+
+      http.get(baseUrl + 'api/features')
+         .subscribe(result => {
+            this.features = result.json() as GetFeatures[];
+         }, error => console.error(error));
+   }
+
+   ngOnInit(): void {
+
+   }
+
+   public loadModel(models) {
+
+      this.models = models;
+
+   }
+}
+
+
+interface GetFeatures {
+   id: string;
+   name: string;
+}
+
+interface GetMakes {
+   id: string;
+   name: string;
+   models: string[];
 }
