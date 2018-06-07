6874c2b73c3354947b8125388514a035abc67253
Pane Atanasovski <atanasovski.pane@gmail.com>
2018-06-07 11:02:55 +0200

    Implement mapping between VehicleResource and Vehicle.

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using Vega.Controllers.Resources;
 using Vega.Models;
@@ -8,9 +9,30 @@ namespace Vega.Mapping
    {
       public MappingProfile()
       {
+         // Domain to API Resource
          CreateMap<Make, MakeResource>();
          CreateMap<Model, ModelResource>();
          CreateMap<Feature, FeatureResource>();
+
+         // API Resource to Domain
+         CreateMap<VehicleResource, Vehicle>()
+            .ForMember(
+               vehicle => vehicle.ContactName,
+               opt => opt.MapFrom(vr => vr.Contact.Name)
+            )
+            .ForMember(
+               vehicle => vehicle.ContactEmail,
+               opt => opt.MapFrom(vr => vr.Contact.Email)
+            )
+            .ForMember(
+               vehicle => vehicle.ContactPhone,
+               opt => opt.MapFrom(vr => vr.Contact.Phone)
+            )
+            .ForMember(
+               vehicle => vehicle.Features,
+               opt => opt.MapFrom(vr =>
+                  vr.Features.Select(id => new VehicleFeature { FeatureId = id }))
+            );
       }
    }
 }
