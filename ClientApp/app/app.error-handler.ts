2523faa2f258eaaabdec78135edacb9a9434679c
Pane Atanasovski <atanasovski.pane@gmail.com>
2018-06-13 18:20:57 +0200

    Handle errors globally.

---
+++ ClientApp/app/app.error-handler.ts
@@ -0,0 +1,20 @@
+import { ErrorHandler, Inject } from '@angular/core';
+import { ToastyService } from 'ng2-toasty';
+
+export class AppErrorHandler implements ErrorHandler {
+
+   constructor(@Inject(ToastyService) private toastyService: ToastyService) { }
+
+   handleError(error: any): void {
+      if (typeof (window) !== 'undefined') {
+         this.toastyService.error({
+            title: 'Error',
+            msg: 'An unexpected error happened!',
+            theme: 'bootstrap',
+            showClose: true,
+            timeout: 5000
+         });
+      }
+   }
+
+}
