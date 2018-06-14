fcc2e1b9a37f6080f6a2905c34207d37fc4ed5c0
Pane Atanasovski <atanasovski.pane@gmail.com>
2018-06-14 14:57:03 +0200

    Refactor: use dictionaries.

--- Persistence/VehicleRepository.cs
+++ Persistence/VehicleRepository.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using Vega.Core;
 using Vega.Core.Models;
@@ -58,29 +60,46 @@ namespace Vega.Persistence
             query = query.Where(v => v.ModelId == queryObj.ModelId.Value);
          }
 
-         if (queryObj.SortBy == "make")
+         var columnsMap = new Dictionary<string, Expression<Func<Vehicle, object>>>()
          {
-            query = (queryObj.IsSortAscending)
-               ? query.OrderBy(v => v.Model.Make.Name) : query.OrderByDescending(v => v.Model.Make.Name);
+            ["make"] = v => v.Model.Make.Name,
+            ["model"] = v => v.Model.Name,
+            ["contactName"] = v => v.ContactName,
+            ["id"] = v => v.Id
+         };
+
+         if (queryObj.IsSortAscending)
+         {
+            query = query.OrderBy(columnsMap[queryObj.SortBy]);
+         }
+         else
+         {
+            query = query.OrderByDescending(columnsMap[queryObj.SortBy]);
          }
 
-         if (queryObj.SortBy == "model")
-         {
-            query = (queryObj.IsSortAscending)
-               ? query.OrderBy(v => v.Model.Name) : query.OrderByDescending(v => v.Model.Name);
-         }
+         // if (queryObj.SortBy == "make")
+         // {
+         //    query = (queryObj.IsSortAscending)
+         //       ? query.OrderBy(v => v.Model.Make.Name) : query.OrderByDescending(v => v.Model.Make.Name);
+         // }
 
-         if (queryObj.SortBy == "contactName")
-         {
-            query = (queryObj.IsSortAscending)
-               ? query.OrderBy(v => v.ContactName) : query.OrderByDescending(v => v.ContactName);
-         }
+         // if (queryObj.SortBy == "model")
+         // {
+         //    query = (queryObj.IsSortAscending)
+         //       ? query.OrderBy(v => v.Model.Name) : query.OrderByDescending(v => v.Model.Name);
+         // }
 
-         if (queryObj.SortBy == "id")
-         {
-            query = (queryObj.IsSortAscending)
-            ? query.OrderBy(v => v.Id) : query.OrderByDescending(v => v.Id);
-         }
+         // if (queryObj.SortBy == "contactName")
+         // {
+         //    query = (queryObj.IsSortAscending)
+         //       ? query.OrderBy(v => v.ContactName) : query.OrderByDescending(v => v.ContactName);
+         // }
+
+         // if (queryObj.SortBy == "id")
+         // {
+         //    query = (queryObj.IsSortAscending)
+         //    ? query.OrderBy(v => v.Id) : query.OrderByDescending(v => v.Id);
+         // }
 
          return query.ToList();
       }
