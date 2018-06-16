0f763bfdb5ac3f0825f5b18c629ad11faf3d5f44
Pane Atanasovski <atanasovski.pane@gmail.com>
2018-06-16 14:36:35 +0200

    Add validation.

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using AutoMapper;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -13,6 +14,10 @@ namespace Vega.Controllers
    [Route("/api/vehicles/{vehicleId}/photos")]
    public class PhotosController : Controller
    {
+      private readonly int MAX_BYTES = 1 * 1024 * 1024;
+
+      private readonly string[] ACCEPTED_FILE_TYPES = new[] { ".jpg", ".jpeg", ".png" };
+
       private readonly IHostingEnvironment host;
 
       private readonly IVehicleRepository repository;
@@ -41,6 +46,26 @@ namespace Vega.Controllers
          if (vehicle == null)
          {
             return NotFound();
+         }
+
+         if (file == null)
+         {
+            return BadRequest("Null file.");
+         }
+
+         if (file.Length == 0)
+         {
+            return BadRequest("Empty file.");
+         }
+
+         if (file.Length > MAX_BYTES)
+         {
+            return BadRequest("Max file size exceeded.");
+         }
+
+         if (!ACCEPTED_FILE_TYPES.Any(s => s == Path.GetExtension(file.FileName)))
+         {
+            return BadRequest("Invalid file type.");
          }
 
          var uploadsFolderPath = Path.Combine(host.WebRootPath, "uploads");
